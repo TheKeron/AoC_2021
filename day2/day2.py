540602b3e9bccbb1acdaa98fea49b9bafc6a7bf5
TheKeron <xkeron19@gmail.com>
2021-12-02 12:22:11 +0100

    Better list comprehension

--- day2/day2.py
+++ day2/day2.py
@@ -1,19 +1,19 @@
 with open('input2', 'r') as f:
-    values = [x for x in f.read().splitlines()]
+    values = [x for x in (x.split(" ") for x in f.read().splitlines())]
 
 def day2():
     h = 0
     d = 0
     aim = 0
     for x in values:
-        split = x.split(' ')
-        if split[0] == 'forward':
-            h += int(split[1])
-            d += (aim * int(split[1]))
-        elif split[0] == 'down':
-            aim += int(split[1])
-        elif split[0] == 'up':
-            aim -= int(split[1])
+        if x[0] == 'forward':
+            h += int(x[1])
+            d += (aim * int(x[1]))
+        elif x[0] == 'down':
+            aim += int(x[1])
+        elif x[0] == 'up':
+            aim -= int(x[1])
     print(h*d)
 
 day2()
+
